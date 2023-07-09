77eab3f2ec51d131a007415fa95fa28c725c1413
Pedro Medeiros <39886834+pedrohdemedeiros@users.noreply.github.com>
2023-07-09 17:58:52 -0300

    Update go_get_your_pdf.py
    Now you have to passa all the arguments in the command line
    Also if a stone takes the place of the other, the second stone will prevail

--- go_get_your_pdf.py
+++ go_get_your_pdf.py
@@ -1,3 +1,4 @@
+import argparse
 import re
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from matplotlib.markers import MarkerStyle
@@ -6,28 +7,28 @@ import matplotlib.patches as patches
 from fpdf import FPDF
 import numpy as np
 
-# Function to extract moves, comments, and alternative moves from the SGF content
-def extract_moves_comments_and_alternatives(sgf_content):
+# Argument parser
+parser = argparse.ArgumentParser(description='Generate a PDF document from a SGF Go game file.')
+parser.add_argument('-i', '--input', type=str, required=True, help='Input SGF file')
+parser.add_argument('-o', '--output', type=str, required=True, help='Output PDF file')
+args = parser.parse_args()
+
+# Function to extract moves and comments from the SGF content
+def extract_moves_and_comments(sgf_content):
     pattern = re.compile(r';(?P<color>[B|W])\[(?P<pos>[a-z][a-z])\](?:C\[(?P<comment>.*?)\])?')
     matches = pattern.findall(sgf_content)
 
     moves = []
     comments = {}
-    alternatives = []
     for i, match in enumerate(matches):
         color, pos, comment = match
         moves.append((color, (ord(pos[0]) - 96, ord(pos[1]) - 96)))
         if comment:
             comments[i] = comment.replace('\n', ' ')
-            # Check for alternative moves in comment
-            alt_moves = re.findall(r'\((?P<color>[B|W])\[(?P<pos>[a-z][a-z])\]\)', comment)
-            for alt_move in alt_moves:
-                alt_color, alt_pos = alt_move
-                alternatives.append((i, (alt_color, (ord(alt_pos[0]) - 96, ord(alt_pos[1]) - 96))))
-    return moves, comments, alternatives
+    return moves, comments
 
 # Function to create an image of the Go board with the given moves
-def create_board_image(board, moves):
+def create_board_image(board, move_numbers, moves):
     fig, ax = plt.subplots(figsize=(8, 8))
     ax.add_patch(patches.Rectangle((-1, -1), 20, 20, facecolor='burlywood'))
     for i in range(19):
@@ -38,11 +39,16 @@ def create_board_image(board, moves):
         ax.plot(x, y, marker='o', markersize=10, color='black')
     for i in range(len(moves)):
         color, (x, y) = moves[i]
-        stone_color = 'white' if color == 'W' else 'black'
-        text_color = 'black' if color == 'W' else 'white'
-        marker = MarkerStyle('o', fillstyle='full')
-        ax.plot(x-1, 19-y, marker=marker, markersize=20, color=stone_color, markeredgecolor='black')
-        ax.text(x-1, 19-y, str(i+1), fontsize=10, ha='center', va='center', color=text_color)
+        board[x-1, 19-y] = color  # Update board state
+        move_numbers[x-1, 19-y] = i + 1  # Update move numbers
+    for y in range(19):
+        for x in range(19):
+            if board[x, y] != '.':
+                stone_color = 'white' if board[x, y] == 'W' else 'black'
+                text_color = 'black' if board[x, y] == 'W' else 'white'
+                marker = MarkerStyle('o', fillstyle='full')
+                ax.plot(x, 18-y, marker=marker, markersize=20, color=stone_color, markeredgecolor='black')
+                ax.text(x, 18-y, str(move_numbers[x, y]), fontsize=10, ha='center', va='center', color=text_color)
     ax.set_aspect('equal', 'box')
     ax.set_xlim(-1, 19)
     ax.set_ylim(-1, 19)
@@ -51,24 +57,21 @@ def create_board_image(board, moves):
     return fig, ax
 
 # Read the SGF file
-with open("game1.sgf", "r") as file:
+with open(args.input, "r") as file:
     sgf_content = file.read()
 
-# Extract moves, comments, and alternative moves from the SGF content
-moves, comments, alternatives = extract_moves_comments_and_alternatives(sgf_content)
+# Extract moves and comments from the SGF content
+moves, comments = extract_moves_and_comments(sgf_content)
+
+# Initialize the board and move_numbers
+board = np.full((19, 19), '.')
+move_numbers = np.full((19, 19), 0)
 
 # Generate high-resolution images for all the moves
 for i in range(len(moves)):
-    fig, ax = create_board_image(np.full((19, 19), '.'), moves[:i+1])
+    fig, ax = create_board_image(board, move_numbers, moves[:i+1])
     canvas = FigureCanvas(fig)
     canvas.print_figure(f"high_res_board_state_{i+1}.png", dpi=100)
-    plt.close(fig)
-
-# Generate high-resolution images for all alternative moves
-for i, (color, (x, y)) in alternatives:
-    fig, ax = create_board_image(np.full((19, 19), '.'), moves[:i+1] + [(color, (x, y))])
-    canvas = FigureCanvas(fig)
-    canvas.print_figure(f"high_res_alternative_{i+1}.png", dpi=100)
     plt.close(fig)
 
 pdf = FPDF()
@@ -76,7 +79,7 @@ pdf.set_font("Arial", size=12)
 
 # Add the introductory message to the first page
 pdf.add_page()
-pdf.cell(0, 10, "This document was made using 'GoGetYourPDF', by Pedro Medeiros. Please, visit my GitHub: https://github.com/pedrohdemedeiros/go_get_your_pdf", ln=True)
+pdf.multi_cell(0, 10, "This document was made with 'GoGetYourPDF', by Pedro Medeiros. Please, visit my GitHub: https://github.com/pedrohdemedeiros/go_get_your_pdf")
 
 for i in range(len(moves)):
     pdf.add_page()
@@ -87,11 +90,5 @@ for i in range(len(moves)):
         pdf.set_xy(10, 210)
         pdf.multi_cell(0, 10, comments[i])
 
-# Add alternative moves to the end of the document
-for i, (color, (x, y)) in alternatives:
-    pdf.add_page()
-    pdf.cell(0, 10, f"Alternative for turn {i+1}", ln=True)
-    pdf.image(f"high_res_alternative_{i+1}.png", x=10, y=30, w=180)
-
-pdf_filename = "go_game_with_high_res_images.pdf"
+pdf_filename = args.output
 pdf.output(pdf_filename)
